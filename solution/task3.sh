21a03708825d946fc1fd6e6cb068cdf1417f0078
w1c1y1 <akulov.max3003@gmail.com>
2025-09-23 14:45:47 +0700

    task3 upd3

--- solution/task3.sh
+++ solution/task3.sh
@@ -2,5 +2,4 @@
 
 # Вывести дату в формате:
 #   13 октября 2023 года, Пятница
-export LC_TIME="ru_RU.UTF-8"
-date '+%d %B %Y года, %A' -d "$@"
+export LC_ALL="ru_RU.UTF-8" date '+%d %B %Y года, %A' -d "$@"
